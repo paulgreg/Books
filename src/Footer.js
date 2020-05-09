69d963709dfb82f3654e1963a0d4d8a9e995a357
Grégory Paul <paulgreg@gmail.com>
2020-05-09 23:37:43 +0200

    Add source to <Footer>

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,26 @@ import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer>
-            <h4 className="Footer-Subtitle">Attributions</h4>
-            Icônes par{' '}
-            <a href="http://ikons.piotrkwiatkowski.co.uk/">
-                Piotr Adam Kwiatkowski
-            </a>
+        <footer className="Footer">
+            <details>
+                <summary>
+                    <h4 className="Footer-Subtitle">Attributions</h4>
+                </summary>
+                <ul className="Footer-List">
+                    <li>
+                        Icônes par{' '}
+                        <a href="http://ikons.piotrkwiatkowski.co.uk/">
+                            Piotr Adam Kwiatkowski
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/paulgreg/Books">
+                            Code on github
+                        </a>{' '}
+                        by <a href="https://paulgreg.me/">paulgreg</a>
+                    </li>
+                </ul>
+            </details>
         </footer>
     )
 }
