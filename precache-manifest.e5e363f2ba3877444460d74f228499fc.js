3e03c8664ec0e028bbdbf0d66fe79e64378d9566
Grégory Paul <paulgreg@gmail.com>
2020-05-10 16:29:26 +0200

    Rebuild GitHub pages

---
+++ precache-manifest.e5e363f2ba3877444460d74f228499fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b96b0fa6fcbb537834c207292ff3831",
+    "url": "/Books/index.html"
+  },
+  {
+    "revision": "83d9edf16a517821efd6",
+    "url": "/Books/static/css/main.c263724b.chunk.css"
+  },
+  {
+    "revision": "ddc798eeea2dae1cc585",
+    "url": "/Books/static/js/2.d7f898d9.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/Books/static/js/2.d7f898d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d9edf16a517821efd6",
+    "url": "/Books/static/js/main.528e81fb.chunk.js"
+  },
+  {
+    "revision": "7da23ea4a9e752f1a3b2",
+    "url": "/Books/static/js/runtime-main.defc50f5.js"
+  },
+  {
+    "revision": "bd85b495e2350c19ce885c3255794c90",
+    "url": "/Books/static/media/arrow_left.bd85b495.svg"
+  },
+  {
+    "revision": "6e23317ca0e049d5be491372d4babaec",
+    "url": "/Books/static/media/document_download.6e23317c.svg"
+  },
+  {
+    "revision": "afd007b9eb5db6368314f4628c22246a",
+    "url": "/Books/static/media/logo.afd007b9.svg"
+  },
+  {
+    "revision": "2b72ebe6592cb5f2b7399fbd9792c0f4",
+    "url": "/Books/static/media/paypal.2b72ebe6.svg"
+  }
+]);
