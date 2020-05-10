1d3437b68fe7ec904061aaa4439a5deb532360be
Grégory Paul <paulgreg@gmail.com>
2020-05-10 12:33:55 +0200

    Activate service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
     <React.StrictMode>
@@ -10,5 +11,4 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-//import * as serviceWorker from './serviceWorker';
-//serviceWorker.register();
+serviceWorker.register()
