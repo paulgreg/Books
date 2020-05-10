3c44c5f3fe80468088334f477e47a6915fda3097
Grégory Paul <paulgreg@gmail.com>
2020-05-10 16:28:38 +0200

    Add language detector

--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,18 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import en from './i18n/en.json'
 import fr from './i18n/fr.json'
 
-i18n.use(initReactI18next).init({
-    resources: { en, fr },
-    fallbackLng: 'en',
-    keySeparator: false,
-    interpolation: {
-        escapeValue: false,
-    },
-})
+i18n.use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        resources: { en, fr },
+        fallbackLng: 'en',
+        keySeparator: false,
+        interpolation: {
+            escapeValue: false,
+        },
+    })
 
 export default i18n
