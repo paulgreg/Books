206ef4bdcee050323c53c46d0555094f5da6f556
Grégory Paul <paulgreg@gmail.com>
2020-05-10 11:42:04 +0200

    Add download link

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './BookList.css'
-import paypal from './paypal.svg'
+import iconPaypal from './paypal.svg'
+import iconDownload from './document_download.svg'
 
 export default function BookList({ bookList, onBookClick }) {
     return (
         <ul className="BookList-BookList">
             {bookList.map((book) => {
-                const { id, title, author, year, donation } = book
+                const { id, title, author, year, donation, epub } = book
 
                 const onClick = (e) => {
                     e.preventDefault()
@@ -28,21 +29,38 @@ export default function BookList({ bookList, onBookClick }) {
                                 {' - '}
                                 <small className="BookList-Year">{year}</small>
                             </a>
+                            {epub && (
+                                <a
+                                    href={epub}
+                                    title="télécharger le fichier epub"
+                                    className="BookList-Action"
+                                >
+                                    <img
+                                        src={iconDownload}
+                                        className="BookList-Icon"
+                                        width="24"
+                                        height="24"
+                                        alt="download"
+                                    />
+                                    télécharger en epub
+                                </a>
+                            )}
                             {donation && (
                                 <a
                                     href={donation}
                                     title="Faire un don via paypal"
-                                    className="BookList-Donation"
+                                    className="BookList-Action"
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
                                     <img
-                                        src={paypal}
-                                        className="BookList-Paypal"
-                                        width="32"
-                                        height="32"
+                                        src={iconPaypal}
+                                        className="BookList-Icon"
+                                        width="24"
+                                        height="24"
                                         alt="paypal"
                                     />
+                                    faire un don
                                 </a>
                             )}
                         </div>
